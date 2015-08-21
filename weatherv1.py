3d28f8179329c50f43d4730916886c8b99dbcc3a
Igor Koltunov <igor.koltunov@gmail.com>
2015-08-21 07:36:40 -0700

    Latest version

--- weatherv1.py
+++ weatherv1.py
@@ -14,14 +14,11 @@ from requests import get
 
 def get_weather_by_zipcode(zipcode):
     '''Input zipcode int. Output json dic.'''
+    
     apiKey = 'ad2e2aeab6b3e474'
     apiCallURLTemplate = 'http://api.wunderground.com/api/{}/conditions/q/{}.json'
     apiCallURL = apiCallURLTemplate.format(apiKey, zipcode)
     resultsDict = dict()
-    
-    # return error if non numeric input 
-    if zipcode.isdigit() == False: 
-        return print('Error: non digit zipcode entered')
          
     results = get(apiCallURL)
     resultsDict =  results.json()
@@ -30,10 +27,22 @@ def get_weather_by_zipcode(zipcode):
 def main():
     
     userZipcode = input('Enter a Zipcode: ')
+
+    # return error if non numeric input 
+    if userZipcode.isdigit() == False: 
+        return print('Error: non digit zipcode entered')
     
     localWeatherDict = get_weather_by_zipcode(userZipcode)
     
-    print(localWeatherDict['current_observation']['temp_f'])
-
-
+    if len(localWeatherDict) != 2 or localWeatherDict['response']['features']['conditions'] != 1:
+        return print('Error: Unexpected localWeatherDict contents')   
+    
+    temperature =  localWeatherDict['current_observation']['temp_f']
+    city = localWeatherDict['current_observation']['display_location']['city']
+    state = localWeatherDict['current_observation']['display_location']['state'] 
+    displayString = 'The current temperature in {}, {} is {}F'.format(city, state, temperature)
+    
+    print(displayString)
+    
+        
 main()
