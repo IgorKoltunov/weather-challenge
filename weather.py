2a4d75dc9263fc78a8179413e7dbbebdb221f51d
Igor Koltunov <igor.koltunov@gmail.com>
2015-08-21 11:48:04 -0700

    Original solution to challenge for (weatherv2.py)

--- weather.py
+++ weather.py
@@ -1,19 +1,28 @@
-''' Part 1: Use the weather underground api to display current 
-    temperature for given zipcode. Zipcode needs to be a global
-    variable. Create a function that takes zipcode as an argument
-    and returns the json. Program should return temp in a user 
-    friendly matter.
+''' Challenge 4.
+    Expand the weather tool to take in a comma-separated list of zip codes as user input. 
+    Validate that each user inputted item is a zip code and if it is, fetch the weather for
+    that zip and print it out the same way as before. Handle some obvious bad user input 
+    conditions. Once all the weather conditions have been displayed to the user, print out 
+    a line telling the user which of the inputed zip codes currently has the warmest weather.
+
+    The data set that should be accounted for includes this '94110,12345,80123,99999,abcde, 90040,4321'
     
-    Script needs to have basic error handling and on sucess print out
-    'The current temperature in <City>, <State> is <temperature in fahrenheit>F'
-    If it fails, it should print out 'There was an error. Try again later'
+    Output example:
+    Enter a comma separated list of zipcodes 23123,123123,123123
+    Feathing data for zipcode x....
+    The temperature in <city>, <State> (zip) is <temp_f>F
+    ...
+    invalid weather data for zipcode...
+    ....
+    invalid zipcode
+    ....
+    City, State (zip) has the warmest weather of temp
 '''
 
-from requests import get
-
+from requests import get, codes
 
 def get_weather_by_zipcode(zipcode):
-    '''Input zipcode int. Output json dic.'''
+    '''Input zipcode int. Output json dict.'''
     
     apiKey = 'ad2e2aeab6b3e474'
     apiCallURLTemplate = 'http://api.wunderground.com/api/{}/conditions/q/{}.json'
@@ -21,28 +30,59 @@ def get_weather_by_zipcode(zipcode):
     resultsDict = dict()
          
     results = get(apiCallURL)
+    
     resultsDict =  results.json()
     return resultsDict
+
+def is_valid_weather(jsonDict):
+    '''original function written by Mark'''
+    if jsonDict is None:
+        return False
+    if 'current_observation' not in jsonDict:
+        return False
+    if 'temp_f' not in jsonDict['current_observation']:
+        return False
+    if 'city' not in jsonDict['current_observation']['display_location']:
+        return False
+    if 'state' not in jsonDict['current_observation']['display_location']:
+        return False
+
+    # All conditions met
+    return True
     
 def main():
     
-    userZipcode = input('Enter a Zipcode: ')
-
-    # return error if non numeric input 
-    if userZipcode.isdigit() == False: 
-        return print('Error: non digit zipcode entered')
+    userZipcodeList = []
+    highestTemp = 0
+    highestDisplayString = ''
     
-    localWeatherDict = get_weather_by_zipcode(userZipcode)
-    
-    if len(localWeatherDict) != 2 or localWeatherDict['response']['features']['conditions'] != 1:
-        return print('Error: Unexpected localWeatherDict contents')   
-    
-    temperature =  localWeatherDict['current_observation']['temp_f']
-    city = localWeatherDict['current_observation']['display_location']['city']
-    state = localWeatherDict['current_observation']['display_location']['state'] 
-    displayString = 'The current temperature in {}, {} is {}F'.format(city, state, temperature)
-    
-    print(displayString)
+    userZipcodeString = input('Enter a comma separated list of zipcodes: ')
+    for str in userZipcodeString.split(','):
+        userZipcode = str.strip()
+        userZipcodeList.append(userZipcode)
+     
+    for userZipcode in userZipcodeList:
+        
+        # return error if non numeric input 
+        if userZipcode.isdigit() == False: 
+            print('Error: non digit zipcode entered:', userZipcode)
+        else:
+            localWeatherDict = get_weather_by_zipcode(userZipcode)
+            
+            # return error if dict contents are unexpected 
+            if is_valid_weather(localWeatherDict) == False:
+                print('Error: Unexpected localWeatherDict contents for:', userZipcode) 
+            else:       
+                temperature =  localWeatherDict['current_observation']['temp_f']
+                city = localWeatherDict['current_observation']['display_location']['city']
+                state = localWeatherDict['current_observation']['display_location']['state'] 
+                if temperature > highestTemp:
+                    highestTemp = temperature
+                    highestDisplayString = '{}, {} {} has the highest temperature of {}F'.format(city, state, userZipcode, temperature)
+                displayString = 'The current temperature in {}, {} {} is {}F'.format(city, state, userZipcode, temperature)
+                print(displayString)
+    if highestDisplayString:
+        print(highestDisplayString)   
     
         
 main()
